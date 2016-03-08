07689f006703e5898d0a4e0e8c388fa704660173
Prakash Gautam <pranphy@gmail.com>
2016-03-08 22:39:45 +0545

    Update settings

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -118,6 +118,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
+
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'static'),)
