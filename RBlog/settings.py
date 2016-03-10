4de0aa075c4fc38578454211e74e14a31df20b89
Prakash Gautam <pranphy@gmail.com>
2016-03-10 19:51:08 +0545

    Make tweak in settings

--- RBlog/settings.py
+++ RBlog/settings.py
@@ -130,5 +130,5 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR,'static'),
-        os.path.join(BASE_DIR,'static/minimal'),
+        os.path.join(BASE_DIR,'static/css/minimal'),
         )
