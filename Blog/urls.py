d269ffbcf407c4f75d1679474cf01a5b8a6b643d
Prakash Gautam <pranphy@gmail.com>
2016-03-23 12:33:33 +0545

    Fix url misconfigurations

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^blog/download$',views.download, name="download"),
     url(r'^(?P<pslug>[\w-]+)$', views.detail, name='detail'),
-    url(r'^download$',views.download, name="download"),
 ]
