60d3c1f0344b66632f45e242d9a23634b337d33b
Prakash Gautam <pranphy@gmail.com>
2016-03-26 23:34:22 +0545

    Generate list of all tags

--- gre/urls.py
+++ gre/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='gre'),
     url(r'^meaning/(?P<pword>[a-z]+)$', views.detail, name='meaning'),
+    url(r'^tag/$',views.alltag, name='alltag'),
     url(r'^tag/(?P<ptag>[\w]+)$',views.tag,name='tag')
-    #url(r'^.+$',views.handler404, name="404"),
 ]
--- gre/views.py
+++ gre/views.py
@@ -27,7 +27,7 @@ def index(request):
     return HttpResponse(template.render(context,request))
 
 def detail(request,pword):
-    currentword = get_object_or_404(Vocab,word=pword) 
+    currentword = get_object_or_404(Vocab,word = pword) 
     template = loader.get_template('gre/details.html')
     context = {
         'pword':pword,
@@ -36,11 +36,24 @@ def detail(request,pword):
     return HttpResponse(template.render(context,request))
 
 def tag(request,ptag):
-    tagids = get_list_or_404(Tag,tagname__contains=ptag)
-    tagwords = get_list_or_404(Vocab,category__in=tagids)
+    tagids = get_list_or_404(Tag,tagname__contains = ptag)
+    tagwords = get_list_or_404(Vocab,category__in = tagids)
     template = loader.get_template('gre/tags.html')
     context = { 
         'tagwords':tagwords,
         'tag' : ptag
     }
     return HttpResponse(template.render(context,request))
+
+def alltag(request):
+    template = loader.get_template('gre/alltags.html')
+    alltags = Tag.objects.all()
+    tagvocab = {}
+    for tag in alltags:
+        allwords = Vocab.objects.filter(category = tag.id)
+        tagvocab[tag.tagname] = allwords
+
+    context = {
+        'tagvocab' : tagvocab
+    }
+    return HttpResponse(template.render(context, request))
