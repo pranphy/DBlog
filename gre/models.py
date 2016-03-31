f70184223bb5c1acf1409a85f3f87dc33eba153f
Prakash Gautam <pranphy@gmail.com>
2016-03-31 19:20:12 +0545

    Add source field in vocab

--- gre/models.py
+++ gre/models.py
@@ -3,6 +3,21 @@ import datetime
 
 from django.utils import timezone
 
+class Source(models.Model):
+    class Type(models.Model):
+        stype = models.CharField(max_length=20,unique=True)
+
+        def __str__(self):
+            return self.stype;
+
+    name =  models.CharField(max_length=20,unique=True)
+    type = models.ManyToManyField('Type', blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Tag(models.Model):
     tagname = models.CharField(max_length=200,unique=True)
 
@@ -16,6 +31,7 @@ class Vocab(models.Model):
     createdate = models.DateTimeField(auto_now_add=True)
     updatedate = models.DateTimeField(auto_now=True)
     category = models.ManyToManyField('Tag',blank=False)
+    source = models.ManyToManyField('Source',blank=False)
     synonym = models.ManyToManyField('Vocab',  blank=True, related_name="%(app_label)s_%(class)s_related_synonym")
     antonym = models.ManyToManyField('Vocab',  blank=True, related_name="%(app_label)s_%(class)s_related_antonym")
     seealso = models.ManyToManyField('Vocab',  blank=True, related_name="%(app_label)s_%(class)s_related_see_also" )
