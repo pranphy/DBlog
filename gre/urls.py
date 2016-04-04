62c941891afb0bc5e5991ad213e597a3b8106628
Prakash Gautam <pranphy@gmail.com>
2016-04-05 00:18:44 +0545

    Switch to CBV in Blog at least

--- gre/urls.py
+++ gre/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='gre'),
+    url(r'^$', views.index, name='index'),
     url(r'^meaning/(?P<pword>[a-z]+)$', views.meaning, name='meaning'),
     url(r'^tag/$',views.alltag, name='alltag'),
     url(r'^tag/(?P<ptag>[\w]+)$',views.tag,name='tag')
