c20c16d649f18df7c250b0720f51c4342521bdfd
Prakash Gautam <pranphy@gmail.com>
2016-04-07 21:45:30 +0545

    Add form test with file upload

--- study/urls.py
+++ study/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.StudyIndex.as_view(), name='index'),
+    url(r'^test/', views.TestView.as_view(), name='test'),
     #url(r'^detail/(?P<pslug>[\w-]+)$', views.BlogDetail.as_view(), name='detail'),
     #url(r'^download$',views.BlogDownload.as_view(), name="download"),
 ]
--- study/views.py
+++ study/views.py
@@ -1,20 +1,54 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from django.utils import timezone
-from django.views.generic import View
-
+import datetime
 
 from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import get_list_or_404
 
 from django.template import RequestContext
+from django.template import loader
 
-import datetime
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import timezone
+from django.views.generic import View
+
+
+from .forms import TestForm
+
+import os
 
 class StudyIndex(View):
     def get(self,request):
         template = loader.get_template('study/index.html')
         context = {}
         return HttpResponse(template.render(context,request))
+
+class TestView(View):
+    def get(self,request):
+        template = loader.get_template('study/test.html')
+        form = TestForm()
+        context = {
+            'form' : form,
+        }
+        return HttpResponse(template.render(context,request))
+
+
+    def handle_upload_file(self,f):
+        # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        with open(BASE_DIR+'/static/images/test','wb+') as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+
+
+    def post(self,request):
+        name = request.POST.get('commenter')
+        email = request.POST.get('email')
+        comment = request.POST.get('comment')
+        form = TestForm(request.POST,request.FILES)
+        receivedfile = request.FILES['uploadfile']
+        self.handle_upload_file(receivedfile)
+        return HttpResponseRedirect('/study/test/')
+
+
