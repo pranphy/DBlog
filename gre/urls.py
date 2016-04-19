487e6b1f7ca1348480cba00462328fc946c7d85c
Prakash Gautam <pranphy@gmail.com>
2016-04-20 00:05:19 +0545

    Implement word search in vocabulary.com

--- gre/urls.py
+++ gre/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.GreIndex.as_view(), name='index'),
     url(r'^meaning/(?P<pword>[a-z]+)$', views.GreMeaning.as_view(), name='meaning'),
     url(r'^tag/$',views.GreAllTag.as_view(), name='alltag'),
-    url(r'^tag/(?P<ptag>[\w]+)$',views.GreTag.as_view(),name='tag')
+    url(r'^tag/(?P<ptag>[\w]+)$',views.GreTag.as_view(),name='tag'),
+    url(r'^test/$',views.TestScrap.as_view(),name='test'),
 ]
--- gre/views.py
+++ gre/views.py
@@ -1,6 +1,11 @@
 #View for the gre app in my blog
+import os
+import requests
 import random
 import datetime
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 from django.http import HttpResponse
 from django.utils import timezone
@@ -65,3 +70,56 @@ class GreAllTag(View):
             'tagvocab' : tagvocab
         }
         return HttpResponse(template.render(context, request))
+
+class TestScrap(View):
+    class WordDefs():
+        short_def = ''
+        long_def = ''
+        def __init__(self,sdef,ldef):
+            self.short_def = sdef
+            self.long_def = ldef
+
+        def set_short_def(self,sdef):
+            self.short_def = sdef
+
+        def set_long_def(self,ldef):
+            self.long_def = ldef
+
+
+
+
+    def allwords(self):
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        with open(BASE_DIR+'/gre/static/media/default.txt') as wordlist:
+            for OneWord in wordlist:
+                Word = OneWord.strip()
+                Word.replace('\n','')
+                yield Word
+
+    def get_def(self,word):
+        url = "http://vocabulary.com/dictionary/"+word
+        soup = BeautifulSoup(urlopen(url))
+        try:
+            shortdef = soup.select('.definitionsContainer .main .section p.short')[0]
+            longdef = soup.select('.definitionsContainer .main .section p.long')[0]
+            return self.WordDefs(str(shortdef),str(longdef))
+        except:
+            return self.WordDefs('not fount','not found')
+
+    def get(self,request):
+        template = loader.get_template('gre/test.html')
+        worddef = {}
+        i = 0
+        for word in self.allwords():
+            i += 1
+            worddef[word] = self.get_def(word)
+
+
+        context = { 
+            'worddef':worddef,
+            'count':i,
+        }
+        return HttpResponse(template.render(context,request))
+
+
+        
